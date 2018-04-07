3c63f6bac5d731f6a29aee6cd7c1984c2bc4b879
suushii <32783057+suushii@users.noreply.github.com>
2018-04-07 15:17:04 -0400

    [hotfix] fix autocomplete

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ module.exports = class DDGScraper {
     async autocomplete(query) {
         const response = await sf.get('https://duckduckgo.com/ac/').query({
             callback: 'autocompleteCallback',
-            q: format(query)
+            q: _format(query)
         })
         return JSON.parse(response.text.replace(/autocompleteCallback\((.+)\);/, "$1"))
     }
