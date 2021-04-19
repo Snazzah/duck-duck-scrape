e130da8daadf0bfb651e2ecf694d1c935b3f6f2f
Snazzah <suggesttosnazzy@gmail.com>
2021-04-19 17:16:16 -0500

    fix(workflow): Fix GPR publish

--- scripts/gpr.ts
+++ scripts/gpr.ts
@@ -3,6 +3,6 @@ import { join } from 'path';
 
 import pkg from '../package.json';
 
-pkg.name = `@dexare/${pkg.name}`;
+pkg.name = `@snazzah/${pkg.name}`;
 
 fs.writeFileSync(join(__dirname, '../package.json'), JSON.stringify(pkg));
