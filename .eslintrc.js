538fdf9fb9570715db3ed99ac7d7484ec14bc698
Snazzah <me@snazzah.com>
2023-02-14 12:43:59 -0600

    fix: fix build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
 module.exports = {
-  ignorePatterns: ['scripts', 'test/__util__/data'],
-  extends: 'snazzah'
+  ignorePatterns: ['scripts', 'test/__util__/data', 'testing'],
+  extends: 'snazzah',
+  overrides: [
+    {
+      files: ['test/**/*.ts'],
+      globals: {
+        describe: true,
+        it: true,
+        beforeEach: true
+      },
+      parserOptions: {
+        sourceType: 'module',
+        project: './tsconfig.eslint.json'
+      }
+    }
+  ]
 };
